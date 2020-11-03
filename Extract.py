4e728b799bda7fec6030b79c126744d0d281b3c2
aliaquil <ali.aquile@gmail.com>
2020-11-03 15:57:59 +0100

    Update Extract.py

--- Extract.py
+++ Extract.py
@@ -2,7 +2,7 @@ import sqlite3
 
 connection = sqlite3.connect("snds_2235.db")
 cursor = connection.cursor()
-cursor.execute("SELECT 'a',* FROM AT_1IJ_V;")
+cursor.execute("SELECT * FROM AT_1IJ_V;")
 results = cursor.fetchall()
 for r in results:
     print(r)
