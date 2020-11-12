9b234a810f47095b9e99b9e94f1dc378d0b13b8e
Abdourahamane BAH <abdourahamane.bah@etudiant.univ-rennes1.fr>
2020-11-12 20:19:55 +0100

    Creating data structure and refonte the existing code

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-from rdflib import Graph, Literal, RDF, URIRef, OWL, Namespace
+from rdflib import Graph, ConjunctiveGraph, Literal, RDF, URIRef, OWL, Namespace
+
 # rdflib knows about some namespaces, like FOAF
 from rdflib.namespace import FOAF , XSD
-import pprint
-# create a graphraph
-graph = Graph()
+import pprint, model
+
 #creating/prefix a namespace
-SNDS = Namespace("http://www.semanticweb.orgraph/user/ontolographies/2020/9/my_ontology#")
+SNDS = Namespace("http://www.localhost.org/")
 CCAM = Namespace('http://www.CCAM.fr/CCAM#')
-ALDCODE =Namespace('http://www.ALDCODE.fr/ALDCode#')
+ALDCODE = Namespace('http://www.ALDCODE.fr/ALDCode#')
 ATC = Namespace('http://www.atc.fr/ATC#')
 UNCAM = Namespace('http://www.UNCAM.fr/UNCAM#')
 CIM10 = Namespace('http://www.CIM10.fr/CIM10#')
 IRNAT = Namespace('http://www.IRNAT.fr/IrNat#')
-HOSPITAL = Namespace('http://www.hospital.fr/hospital#')
-DOCTOR = Namespace('http://www.doctor.fr/doctor#')
-PHARMACY = Namespace('http://www.pharmacy.fr/pharmacy#')
+HOSPITAL =Namespace('http://www.localhost.org/hospital#')
+DOCTOR = Namespace('http://www.localhost.fr/doctor#')
+PHARMACY = Namespace('http://www.localhost.fr/pharmacy#')
 
 ## Definition of the object properties for Patient
 patient = SNDS['Sequence']
@@ -32,7 +32,6 @@ date_start = SNDS['hasDateStart']
 date_end = SNDS['hasDateEnd']
 executor = SNDS['hasExecutor']
 
-
 #class of type of event
 event_ald = SNDS['ALD']
 event_drug_delivery = SNDS['DrugDelivery']
@@ -40,108 +39,89 @@ event_biology = SNDS['Biology']
 event_hospitalisation = SNDS['Hospitalization']
 event_acte = SNDS['Acte']
 event_consultation= SNDS['Consultation']
+## lOADING THE DATA IN THE STRUCTURE
+events1 = []
+events1.append(model.Event(model.Doctor('DO12050','SP0102'),model.Consultation('CONS100254'),'2020-02-15','2020-05-12'))
+events1.append(model.Event(model.Doctor('DO12050','SP0102'),model.Acte('AC150254'),'2020-02-15','2020-05-12'))
+events1.append(model.Event(model.Pharmacy('PH14250'),model.DrugDelevery('DD50254',10),'2020-02-15','2020-05-12'))
+events1.append(model.Event(model.Hospital('HO12050'),model.Hospitalisation('AC150254','DIAG15200'),'2020-02-15','2020-05-12'))
+sequence1 = model.Sequence('PAT125012','1943-01-12','2090-01-12',2,'35700',events1)
+events2 = []
+events2.append(model.Event(model.Doctor('DO12015','SP0111'),model.ALD('CONS100254'),'2020-02-15','2020-05-12'))
+events2.append(model.Event(model.Hospital('HOS12015'),model.Biology('BIO150254'),'2021-02-15','2020-05-12'))
+events2.append(model.Event(model.Doctor('DO12015','SP0111'),model.Consultation('CONS254'),'2020-02-15','2020-05-12'))
+events2.append(model.Event(model.Hospital('HOS12015'),model.Acte('AC15515'),'2021-12-12','2021-05-12'))
+sequence2 = model.Sequence('PAT125000','1950-01-02','2090-01-15',1,'9200',events2)
+sdns_array = []
+sdns_array.append(sequence1)
+sdns_array.append(sequence2)
+############################################################################################################################
 
+## CREATING THE RDF FILE
+graph1 = Graph()
+for seq in sdns_array:
+    patient = SNDS[seq.refSequence]
+    graph1.add((patient, RDF.type, patient))
+    graph1.add((patient, gender, Literal(seq.gender)))
+    graph1.add((patient, location_code, Literal(seq.localisation, datatype = XSD['string'])))
+    graph1.add((patient, date_of_birth, Literal(seq.dateofBirth, datatype = XSD['date'])))
+    graph1.add((patient, date_of_death, Literal(seq.dateofDeath, datatype = XSD['date'])))
+    i = 0
+    for evt in seq.events:
+        i = i + 1
+        event_i = patient + "evt" + str(i)
+        graph1.add((patient, is_composed_of, event_i))
+        graph1.add((event_i, RDF.type, event))
+        graph1.add((event_i, date_start, Literal(evt.dateStart, datatype = XSD['date'])))
+        graph1.add((event_i, date_end, Literal(evt.dateEnd, datatype = XSD['date'])))
+        if(isinstance(evt.evenType,model.ALD)):
+            graph1.add((event_i, has_type, event_drug_delivery))
+            graph1.add((event_i, hasLabel, ALDCODE[evt.evenType.label]))
+        elif(isinstance(evt.evenType,model.DrugDelevery)):
+            graph1.add((event_i, has_type, event_drug_delivery))
+            graph1.add((event_i, hasLabel, ATC[evt.evenType.label]))
+            graph1.add((ATC[evt.evenType.label], SNDS['quantity'],Literal(evt.evenType.quantity)))
+        elif(isinstance(evt.evenType,model.Acte)):
+            graph1.add((event_i, has_type, event_acte))
+            graph1.add((event_i, hasLabel, CCAM[evt.evenType.label]))
+        elif(isinstance(evt.evenType,model.Biology)):
+            graph1.add((event_i, has_type, event_biology))
+            graph1.add((event_i, hasLabel, UNCAM[evt.evenType.label]))
+        elif(isinstance(evt.evenType,model.Consultation)):
+            graph1.add((event_i, has_type, event_consultation))
+            graph1.add((event_i, hasLabel, IRNAT[evt.evenType.label]))
+        elif(isinstance(evt.evenType,model.Hospitalisation)):
+            graph1.add((event_i, has_type, event_hospitalisation))
+            graph1.add((event_i, hasLabel, CIM10[evt.evenType.label]))
+            graph1.add((CIM10[evt.evenType.label], SNDS['diagnosis'],Literal(evt.evenType.diagnosis)))
 
+        if isinstance(evt.executer,model.Hospital):
+            graph1.add((event_i, executor, HOSPITAL[evt.executer.label]))
+        elif isinstance(evt.executer,model.Doctor):
+            graph1.add((event_i, executor, DOCTOR[evt.executer.label]))
+            graph1.add((DOCTOR[evt.executer.label], SNDS['hasSpeciality'], Literal(evt.executer.speciality, datatype = XSD['string'])))
+        elif isinstance(evt.executer,model.Pharmacy):
+            graph1.add((event_i, executor,PHARMACY[evt.executer.label]))
 
-# Definition of the data properties for patient1
-patient1 = SNDS['Patient1']
-patient2 = SNDS['Patient2']
-# Definition of event
-Event0 = SNDS['Patient0Evt1']
-Event1 = SNDS['Patient0Evt2']
-Event2 = SNDS['Patient1Evt3']
-Event3 = SNDS['Patient1Evt4']
-Event4 = SNDS['Patient1Evt5']
-Event5 = SNDS['Patient1Evt6']
+############################################################################################################################################
+## fix the prefix on the head of the file
+graph1.bind("foaf", FOAF)
+graph1.bind("snds", SNDS)
+graph1.bind("atc", ATC)
+graph1.bind("ccam",CCAM)
+graph1.bind("cim10", CIM10)
+graph1.bind("ald",ALDCODE)
+graph1.bind("uncam",UNCAM)
+graph1.bind("irnat",IRNAT)
+graph1.bind("hosp", HOSPITAL)
+graph1.bind("pharm", PHARMACY)
+graph1.bind("doctor", DOCTOR)
 
-
-# Add triples usingraph store's add() method for sequence patient1
-graph.add((patient1, RDF.type, patient))
-graph.add((patient1, gender, Literal(1)))
-graph.add((patient1, location_code, Literal('35700', datatype = XSD['string'])))
-graph.add((patient1, date_of_birth, Literal('1943-02-28', datatype = XSD['date'])))
-graph.add((patient1, date_of_death, Literal('2090-02-28', datatype = XSD['date'])))
-
-graph.add((patient1, is_composed_of, Event0))
-graph.add((Event0, RDF.type, event))
-graph.add((Event0, hasLabel, ATC['N02BE0']))
-graph.add((Event0, date_start, Literal('2020-02-28', datatype = XSD['date'])))
-graph.add((Event0, date_end, Literal('2020-07-28', datatype = XSD['date'])))
-graph.add((Event0, executor, HOSPITAL['22100450']))
-graph.add((Event0, has_type, event_drug_delivery))
-#graph.add((event_drug_delivery, hasLabel,ATC['N02BE0']))
-#graph.add((event_drug_delivery, SNDS['quantity'],Literal(20)))
-
-graph.add((patient1, is_composed_of, Event1))
-graph.add((Event1, RDF.type, event))
-graph.add((Event1, hasLabel, CCAM['YYYY600']))
-graph.add((Event1, date_start, Literal('2020-01-28', datatype = XSD['date'])))
-graph.add((Event1, date_end, Literal('2020-05-28', datatype = XSD['date'])))
-graph.add((Event1, has_type, event_hospitalisation))
-graph.add((Event1, executor, HOSPITAL['22100450']))
-
-#hospital attributes
-#Hospitalisation attribute
-#graph.add((event_hospitalisation, SNDS['diagnosis'], CIM10['IH1152000']))
-#graph.add((event_hospitalisation, hasLabel, CCAM['YYYY600']))
-
-
-# Add triples usingraph store's add() method for patient2
-graph.add((patient2, RDF.type, patient))
-graph.add((patient2, gender, Literal(2)))
-graph.add((patient2, location_code, Literal('35700', datatype = XSD['string'])))
-graph.add((patient2, date_of_birth, Literal('1943-02-28', datatype = XSD['date'])))
-graph.add((patient2, date_of_death, Literal('2090-02-28', datatype = XSD['date'])))
-
-graph.add((patient2, is_composed_of, Event2))
-graph.add((Event2, hasLabel, UNCAM['UNCAM00600']))
-graph.add((Event2, date_start, Literal('2010-01-28', datatype = XSD['date'])))
-graph.add((Event2, date_end, Literal('2010-05-28', datatype = XSD['date'])))
-graph.add((Event2, has_type, event_consultation))
-graph.add((Event2, executor, DOCTOR['501100450']))
-graph.add((DOCTOR['501100450'],SNDS['hasSpeciality'], Literal('Ophtalmologie', datatype = XSD['string'])))
-
-graph.add((patient2, is_composed_of, Event3))
-graph.add((Event3, hasLabel, ALDCODE['ALD0152600']))
-graph.add((Event3, date_start, Literal('2020-01-28', datatype = XSD['date'])))
-graph.add((Event3, date_end, Literal('2020-05-28', datatype = XSD['date'])))
-graph.add((Event3, has_type, event_ald))
-graph.add((Event3, executor, DOCTOR['22100450']))
-graph.add((DOCTOR['22100450'],SNDS['hasSpeciality'], Literal('Cancerologie', datatype = XSD['string'])))
-
-graph.add((patient2, is_composed_of, Event4))
-graph.add((Event4, hasLabel, IRNAT['IRNAT52600']))
-graph.add((Event4, date_start, Literal('2020-11-28', datatype = XSD['date'])))
-graph.add((Event4, date_end, Literal('2020-12-28', datatype = XSD['date'])))
-graph.add((Event4, has_type, event_hospitalisation))
-graph.add((Event4, executor, HOSPITAL['501100450']))
-
-graph.add((patient2, is_composed_of, Event5))
-graph.add((Event5, hasLabel, CIM10['CIM102600']))
-graph.add((Event5, date_start, Literal('2018-01-28', datatype = XSD['date'])))
-graph.add((Event5, date_end, Literal('2019-05-28', datatype = XSD['date'])))
-graph.add((Event5, has_type, event_acte))
-graph.add((Event5, executor, DOCTOR['22100450']))
-graph.add((DOCTOR['22100450'],SNDS['hasSpeciality'], Literal('Cancerologie', datatype = XSD['string'])))
-
-# Bind all namespaces to a prefix for more readable output
-graph.bind("foaf", FOAF)
-graph.bind("snds", SNDS)
-graph.bind("atc", ATC)
-graph.bind("ccam",CCAM)
-graph.bind("cim10", CIM10)
-graph.bind("ald",ALDCODE)
-graph.bind("uncam",UNCAM)
-graph.bind("irnat",IRNAT)
-graph.bind("hosp", HOSPITAL)
-graph.bind("pharm", PHARMACY)
-graph.bind("doctor", DOCTOR)
 
 # print all the data in the xml format
 print("--- print all the data in the xml format ---")
-print(graph.serialize(format='ttl').decode("utf-8"))
+print(graph1.serialize(format='ttl').decode("utf-8"))
 
 
 ## Saving the created graph into the file with turtle format
-graph.serialize(destination='output.ttl', format='turtle')
-
+graph1.serialize(destination='output3.ttl', format='turtle')
